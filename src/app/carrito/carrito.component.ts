2936725631b14fa79357fd685dc1e0b15f2450c6
DIEGODAZA619 <diegodaza.da619@gmail.com>
2022-10-31 22:00:53 -0400

    EJERCICIOS

--- src/app/carrito/carrito.component.ts
+++ src/app/carrito/carrito.component.ts
@@ -10,6 +10,8 @@ export class CarritoComponent implements OnInit
   titulo:string;
   estado:boolean;
   categorias: any = [];
+  productos: any = [];
+  cant_venta:number = 1; 
 
   constructor() {
     this.titulo = "CARRITO DIEGO DE COMPRAS"
@@ -17,10 +19,28 @@ export class CarritoComponent implements OnInit
     this.categorias.push("ROPA");
     this.categorias.push("MUEBLES");
     this.categorias.push("JUGUETES");
+    //-------------
+    this.productos.push({
+                          nombre:"CHAMARRA",
+                          precio: 250,
+                          cantidad: 12
+                        });
+    this.productos.push({
+                          nombre:"PELOTA",
+                          precio: 100,
+                          cantidad: 3
+                        }); 
+    this.productos.push({
+                          nombre:"sombrero",
+                          precio: 100,
+                          cantidad: 3
+                        });                        
    }
 
   ngOnInit(): void {
   }
+
+
   cerrar_tienda()
   {
     if(this.estado)
@@ -30,8 +50,16 @@ export class CarritoComponent implements OnInit
     else
     {
       this.estado = true;
-    }
-    
+    }    
+  }
+  cambio(data : any)
+  {
+    this.cant_venta = data.target.value;
+  }
+
+  eliminar_producto(i : number)
+  {
+    this.productos.splice(i, 1);
   }
 }
 
