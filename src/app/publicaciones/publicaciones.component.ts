b08ef0478d8e8ff486f42397ea18c341f70ae8e3
DIEGODAZA619 <diegodaza.da619@gmail.com>
2022-11-10 22:06:22 -0400

    PUBLICACIONES

---
+++ src/app/publicaciones/publicaciones.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { PublicacionesService } from './publicaciones.service';
+
+@Component({
+  selector: 'app-publicaciones',
+  templateUrl: './publicaciones.component.html',
+  styleUrls: ['./publicaciones.component.css']
+})
+export class PublicacionesComponent implements OnInit {
+   
+  datos:any = [];
+  constructor(protected publicaciones: PublicacionesService)
+  {
+    this.publicaciones.lista_publicaciones().subscribe
+    (
+      (res:any) => {
+        console.log(res);
+        this.datos = res;
+      },
+      (error:any)=>{
+        console.log(error);
+      }
+    );
+
+  }
+
+  ngOnInit(): void {
+  }
+
+}
